3890479c01873801c9cdacb8c8b50cf349725088
BrunoSantosCosta <brunosanttos667@gmail.com>
2021-05-29 21:07:37 -0300

    Manipulação dos elementos aparecem apenas ao iniciar o jogo

--- js.js
+++ js.js
@@ -1,19 +1,36 @@
-const imgs = document.getElementsByTagName('img');
-
-function mostrar() {
-  for (var i = 0; i < imgs.length; i++) {
-    if (imgs[i].style.display == 'none' || imgs[i].style.display == '') {
-      imgs[i].style.display = 'inline-block';
-      break;
-    }
-  }
+function invisivel(elements) {
+  const display = document.getElementById(elements).style.display;
+  if (display == 'none') {
+    document.getElementById(elements).style.display = 'none';
+  } else document.querySelector('.inicio').style.display = 'none';
+  document.getElementById(elements).style.display = 'block';
 }
 
-function apagar() {
-  for (var i = imgs.length - 1; i >= 0; i--) {
-    if (imgs[i].style.display == 'inline-block') {
-      imgs[i].style.display = 'none';
-      break;
-    }
+function play() {
+  const score = 0;
+  const jogadorVida = 4;
+  const posicaoX1 = parseInt(Math.random() * 1250);
+  const posicaoX2 = parseInt(Math.random() * 1250);
+  const posicaoX3 = parseInt(Math.random() * 1250);
+  const posicaoX4 = parseInt(Math.random() * 1250);
+  const posicaoX5 = parseInt(Math.random() * 1250);
+  const posicaoX6 = parseInt(Math.random() * 1250);
+  const posicaoX7 = parseInt(Math.random() * 1250);
+  const velocidade = 3;
+
+  const TECLA = {
+    W: 87,
+    S: 83,
+    D: 68,
+    A: 65,
+    SD: 39,
+    SE: 37,
+  };
+
+  const game = {};
+  game.pressionou = [];
+
+  function timer() {
+    game.timer = setInterval(loop, 10);
   }
 }
