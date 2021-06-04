fb924581142cb32fe8eee2d5177dbad110f88344
BrunoSantosCosta <brunosanttos667@gmail.com>
2021-06-04 09:37:16 -0300

    alterações

--- js.js
+++ js.js
@@ -1,61 +1,50 @@
-function invisivel(elements) {
-  const display = document.getElementById(elements).style.display;
-  if (display == 'none') {
-    document.getElementById(elements).style.display = 'none';
-  } else document.querySelector('.inicio').style.display = 'none';
-  document.getElementById(elements).style.display = 'block';
-}
+let dirxJ, velJ, pjx;
+let tamTelaW, tamTelaH;
+let jogo;
+let frames;
 
-function play() {
-  var score = 0;
-  var jogadorVida = 4;
-  var posicaoX1 = parseInt(Math.random() * 1250);
-  var posicaoX2 = parseInt(Math.random() * 1250);
-  var posicaoX3 = parseInt(Math.random() * 1250);
-  var posicaoX4 = parseInt(Math.random() * 1250);
-  var posicaoX5 = parseInt(Math.random() * 1250);
-  var posicaoX6 = parseInt(Math.random() * 1250);
-  var posicaoX7 = parseInt(Math.random() * 1250);
-  var velocidade = 3;
-
-  const game = {};
-  game.pressionou = [];
-
+function controlaJogador() {
   let obj = document.getElementById('ruffles');
   let px = 0;
   window.addEventListener('keydown', (e) => {
     let tecla = e.key;
-    switch (tecla) {
-      case 'ArrowRight':
-        px += 80;
+    if (tecla == 'ArrowRight') {
+      px += 30;
 
-        obj.style.transform = `translateX(${px}px)`;
-        break;
-      case 'ArrowLeft':
-        px -= 80;
-        obj.style.transform = `translateX(${px}px)`;
-        break;
+      obj.style.transform = `translateX(${px}px)`;
+    } else if (tecla == 'ArrowLeft') {
+      px -= 30;
+      obj.style.transform = `translateX(${px}px)`;
+    } else {
     }
   });
+  gameLoop();
+}
+function criaComidas() {
+  let comidas_na_tela = document.getElementsByClassName('food');
+  comidas_na_tela = comidas_na_tela.length;
+  if (jogando && bmb_totais > 0 && bombas_na_tela < 5) {
+    let x = Math.random() * 440;
+    let y = -60;
+    x = parseInt(x);
 
-  function loop() {
-    moveJogador();
-    moveComida1();
-    moveComida2();
-    //moveComida3();
-    //moveComida4();
-    moveVeneno1();
-    moveVeneno2();
-    colisao();
-    pontos();
-    vida();
-  }
-
-  function criaComida() {
-    if (play) {
-      var y = 0;
-      var x = Math.random() * 1250;
-      var comida = document.createElementNS('');
-    }
+    let tela_comida = document.getElementsByClassName('food');
+    tela_comida.innerHTML += `<div class="food" style="background-position: ${x}px ${y}px;"></div>`;
   }
 }
+
+function gameLoop() {
+  if (jogo) {
+    controlaJogador();
+  }
+  frames = requestAnimationFrame(gameLoop);
+}
+
+function play() {
+  jogo = true;
+
+  gameLoop();
+}
+
+window.addEventListener('load', play);
+tamTelaW = window.innerWidth;
